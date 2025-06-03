82b39be40864438f93a9440a8f0ae2e0ffae6d53
Denis Lukin <denislukinkuopio@yandex.com>
2025-06-03 10:42:32 +0300

    init app base

---
+++ components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div className="header">
+    <div className="menu"><UButton /></div>
+    <div className="Buttons"><ColorModeButton /></div>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  width: 100%;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  padding: 24px;
+  justify-content: space-between;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,41 @@
-<script setup lang="ts">
-import ruMessages from './locale.ru.json';
-import enMessages from './locale.en.json';
-const { t } = useI18n({
-  messages: {
-    ru: ruMessages,
-    en: enMessages,
-  },
-});
+<script setup>
+import { useScriptTag } from '@vueuse/core';
+
+const colorMode = useColorMode();
+
+useScriptTag(
+  'https://cdnjs.cloudflare.com/ajax/libs/annyang/2.6.1/annyang.min.js',
+  () => {
+    if (annyang) {
+      const commands = {
+        'use dark': () => {
+          colorMode.preference = 'dark';
+        },
+        'use light': () => {
+          colorMode.preference = 'light';
+        },
+        'test *term': (term) => {
+          alert(term);
+        },
+        hello: () => {
+          alert('Hello world!');
+        },
+      };
+
+      // Add our commands to annyang
+      annyang.addCommands(commands);
+
+      // Start listening.
+      // annyang.start();
+    }
+  }
+);
 </script>
 <template>
-  <AppHeader />
+  <Header />
   <SidebarMenu />
-  <UButton color="error">{{ t('name') }}</UButton>
-  <UButton color="secondary">{{ $t('pages.layout.welcome') }}</UButton>
-  <slot />
-  <ColorModeButton />
+  <!-- <UButton color="error">{{ t('name') }}</UButton>
+  <UButton color="secondary">{{ $t('pages.layout.welcome') }}</UButton> -->
+  <!-- <slot /> -->
 </template>
+<style scoped></style>
